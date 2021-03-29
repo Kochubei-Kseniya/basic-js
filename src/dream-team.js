8b2905ad269dc5b33df8a4fae4cab360f2b7fbee
Kochubei Kseniya <79758262+Kochubei-Kseniya@users.noreply.github.com>
2021-03-29 12:27:03 +0300

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  let dreamTeamName = [];
+  if (!Array.isArray(members)) return false;
+  members.forEach((member) => {
+    if (typeof member == "string") {
+      dreamTeamName.push(member.trim()[0].toUpperCase());
+    }
+    return dreamTeamName;
+  });
+ if (dreamTeamName.length > 0) 
+ return dreamTeamName.sort().join("");
+ else return false;
+ };
